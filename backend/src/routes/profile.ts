c08641097920d1101d5aec9e798b65ec09452dc4
rjtormis <raneljohnt@gmail.com>
2024-02-14 21:29:19 +0800

    Updated controllers

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -1,5 +1,12 @@
+import { getSpecificProfile, updateSpecificProfile } from "@controllers/profile";
+import { userIdValidator, userValidator } from "@middlewares/user";
 import { Router } from "express";
 
 const profileRouter = Router();
 
-profileRouter.route("/:id").get();
+profileRouter
+  .route("/:id")
+  .get(userIdValidator, getSpecificProfile)
+  .patch(userValidator, updateSpecificProfile);
+
+export default profileRouter;
