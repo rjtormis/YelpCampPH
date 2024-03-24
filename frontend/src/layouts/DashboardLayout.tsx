31c0874a5c3f9d03334b213d248459785dcc39ad
rjtormis <raneljohnt@gmail.com>
2024-03-24 19:12:02 +0800

    added dashboard design

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,59 +1,98 @@
 import logo from "@/assets/logo.png";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
-import { FlameKindling } from "lucide-react";
+import { Home, List, LogOut, Mail, Moon, Settings, SquareUser } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 function DashboardLayout() {
+  const location = useLocation();
+
   return (
-    <div className="w-screen h-screen p-10">
+    <div className="w-screen h-screen ">
       <header className="flex  items-center">
         <div className="flex items-center">
           <img src={logo} alt="YelpCampPH" className="h-[60px] w-[60px]" />
           <p className="text-xl font-semibold">YelpCamp PH</p>
         </div>
       </header>
-      <Outlet />
-      <ResizablePanelGroup direction="horizontal" className=" min-h-[200px] rounded-lg border">
+      <ResizablePanelGroup direction="horizontal" className="  border">
         <ResizablePanel defaultSize={12}>
           <div className="flex h-full  p-6">
-            <ul>
-              <li className="flex content-center justify-center">
-                <Button variant="ghost">
-                  <FlameKindling size={18} className="m-auto" /> <p className="ml-4">Campsites</p>
-                </Button>
+            <ul className="w-full">
+              <li className="w-full content-center justify-left">
+                <Link to="/dashboard">
+                  <Button
+                    variant={location.pathname === "/dashboard" ? "default" : "ghost"}
+                    className="w-full flex justify-start"
+                  >
+                    <Home size={18} className="mr-4" /> <p className="">Home</p>
+                  </Button>
+                </Link>
               </li>
               <li className=" w-full content-center justify-center">
-                <Button variant="ghost" className="w-full">
-                  <FlameKindling size={18} className="m-auto" /> <p className="ml-4">Inbox</p>
-                </Button>
+                <Link to="/dashboard/listing">
+                  <Button
+                    variant={location.pathname === "/dashboard/listing" ? "default" : "ghost"}
+                    className="w-full flex justify-start"
+                  >
+                    <List size={18} className="mr-4" />
+                    <p className="">Listing</p>
+                  </Button>
+                </Link>
+              </li>
+              <li className=" w-full content-center justify-start">
+                <Link to="/dashboard/inbox">
+                  <Button
+                    variant={location.pathname === "/dashboard/inbox" ? "default" : "ghost"}
+                    className="w-full flex justify-start"
+                  >
+                    <Mail size={18} className="mr-4" /> <p className="">Inbox</p>
+                  </Button>
+                </Link>
               </li>
               <li className=" content-center justify-center">
-                <Button variant="ghost" className="w-full">
-                  <FlameKindling size={18} className="m-auto" /> <p className="ml-4">Profile</p>
-                </Button>
+                <Link to="/dashboard/profile">
+                  <Button
+                    variant={location.pathname === "/dashboard/profile" ? "default" : "ghost"}
+                    className="w-full flex justify-start"
+                  >
+                    <SquareUser size={18} className="mr-4" /> <p className="">Profile</p>
+                  </Button>
+                </Link>
               </li>
               <li className=" content-center justify-center">
-                <Button variant="ghost" className="w-full">
-                  <div>
-                    <FlameKindling size={18} className="m-auto" />
-                  </div>
-                  <p className="ml-4">Setting</p>
-                </Button>
+                <Link to="/dashboard/setting">
+                  <Button
+                    variant={location.pathname === "/dashboard/setting" ? "default" : "ghost"}
+                    className="w-full flex justify-start"
+                  >
+                    <Settings size={18} className="mr-4" />
+                    <p className="">Setting</p>
+                  </Button>
+                </Link>
               </li>
-              <li className=" content-center justify-center">
-                <Button variant="ghost" className="w-full">
-                  <FlameKindling size={18} className="m-auto" /> <p className="ml-4">Logout</p>
+
+              <li className=" w-full content-center justify-center m-auto">
+                <Separator className="my-4" />
+                <Button variant="ghost" className="w-full flex justify-start">
+                  <Moon size={18} className="mr-4" />
+                  <p className="">Theme</p>
+                </Button>
+
+                <Button variant="ghost" className="w-full flex justify-start">
+                  <LogOut size={18} className="mr-4" /> <p className="">Logout</p>
                 </Button>
               </li>
             </ul>
           </div>
         </ResizablePanel>
-        <ResizableHandle withHandle />
+        <ResizableHandle />
         <ResizablePanel className="" defaultSize={75}>
-          <div className="flex h-full items-center justify-center p-6">
+          <Outlet />
+          {/* <div className="flex h-full items-center justify-center p-6">
             <span className="font-semibold">Content</span>
-          </div>
+          </div> */}
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
